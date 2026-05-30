8d59101d9514fc8d1ec91a622ae04ad7a82a56be
Thomas Ng <thomas.ng@nikhef.nl>
2026-05-31 01:54:59 +0200

    fix: remove log propagation

--- src/jimgw/__init__.py
+++ src/jimgw/__init__.py
@@ -9,11 +9,8 @@ except PackageNotFoundError:
 
 # Configure jimgw logging on import so all components (Jim class, samplers,
 # CLI, standalone modules) produce INFO output without any application setup.
-# propagate=False isolates jimgw from the root logger to avoid duplicates
-# when the application also configures logging.
 _log = logging.getLogger(__name__)
 _log.setLevel(logging.INFO)
-_log.propagate = False
 if not any(isinstance(h, logging.StreamHandler) for h in _log.handlers):
     _h = logging.StreamHandler()
     _h.setFormatter(logging.Formatter("%(name)s - %(levelname)s - %(message)s"))

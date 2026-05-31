07d050e807ff865670ecd507c56e6ac8c0aed8a9
Thomas Ng <thomas.ng@nikhef.nl>
2026-05-31 12:28:58 +0200

    refactor: remove redundant default configuration tests for FlowMC and BlackJAX samplers

--- tests/unit/samplers/test_config.py
+++ tests/unit/samplers/test_config.py
@@ -17,42 +17,6 @@ from jimgw.samplers.config import (
 )
 
 
-def test_flowmc_config_defaults():
-    cfg = FlowMCConfig()
-    assert cfg.type == "flowmc"
-    assert cfg.n_chains == 1000
-    assert cfg.local_kernel == "MALA"
-    assert cfg.mala.step_size == 2e-3
-    assert cfg.parallel_tempering is None
-    assert cfg.checkpoint_dir is None
-    assert cfg.checkpoint_interval == 0.0
-
-
-def test_blackjax_ns_aw_config_defaults():
-    cfg = BlackJAXNSAWConfig()
-    assert cfg.type == "blackjax-ns-aw"
-    assert cfg.n_live == 1000
-    assert cfg.n_delete_frac == 0.5
-    assert cfg.checkpoint_dir is None
-    assert cfg.checkpoint_interval == 0.0
-
-
-def test_blackjax_nss_config_defaults():
-    cfg = BlackJAXNSSConfig()
-    assert cfg.type == "blackjax-nss"
-    assert cfg.n_live == 2000
-    assert cfg.checkpoint_dir is None
-    assert cfg.checkpoint_interval == 0.0
-
-
-def test_blackjax_smc_config_defaults():
-    cfg = BlackJAXSMCConfig()
-    assert cfg.type == "blackjax-smc"
-    assert cfg.n_particles == 5000
-    assert cfg.checkpoint_dir is None
-    assert cfg.checkpoint_interval == 0.0
-
-
 def test_discriminated_union_dispatch_flowmc():
     cfg = FlowMCConfig.model_validate({"type": "flowmc", "n_chains": 500})
     assert isinstance(cfg, FlowMCConfig)
